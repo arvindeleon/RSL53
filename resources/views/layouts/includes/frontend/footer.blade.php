ea1b9c62ce684f49afa4a932807df4accc1bb91c
arvindeleon <Arvin@nayloroutsourcing.com>
2016-12-22 00:40:35 +0800

    Amends

--- resources/views/layouts/includes/frontend/footer.blade.php
+++ resources/views/layouts/includes/frontend/footer.blade.php
@@ -1,5 +1,14 @@
   <div class="px-footer px-footer-bottom" id="footer-bottom">
 	<div class="container">
-		<small>Copyright © Arvin Shelby H. de Leon, 2016. All Rights Reserved</small>
+		<div class="social-tmb pull-left">
+			<ul>
+				<li><a href="#"><i class="fa fa-lg fa-facebook-square"></i></a></li>
+				<li><a href="#"><i class="fa fa-lg fa-twitter-square"></i></a></li>
+				<li><a href="#"><i class="fa fa-lg fa-google-plus-square"></i></a></i></li>
+			</ul>
+		</div>
+		<div class="copyright pull-right">
+			<small>Copyright © Arvin Shelby H. de Leon, 2016. All Rights Reserved</small>
+		</div>
 	</div>
   </div>
